f36d44a14edde24bdb5ced50898475b7aaf9fd59
lolow63 <laurentbrugiere@yahoo.fr>
2015-01-27 02:30:50 +0100

    création de construction.php

---
+++ construction.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Institut Français | Algérie</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <link rel="stylesheet" href="css/dinot.css">
+        <link rel="stylesheet" href="css/fa/font-awesome.min.css">
+        <link rel="stylesheet" href="css/style.css">
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	<body>
+		<header>
+			<?php include("include/header-login.php");
+			?>
+		</header>
+		<div class="jumbotron">
+			<div class="subheader">
+				<div class="container">
+					<h1 class="text-center"><span class="fa fa-cube pull-left"></span>EN CONSTRUCTION<span class="fa fa-cubes pull-right"></span></h1>
+				</div>
+			</div>
+		</div>
+		<hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr><hr>
+		<footer>
+			<?php include("include/footer.php"); ?> 
+		</footer>
+	</body>
+<html>
