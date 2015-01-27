e5c8ac1cc2a28bb94b89cd6541c24d705de6f5c6
lolow63 <laurentbrugiere@yahoo.fr>
2015-01-27 02:32:51 +0100

    création de corrige@.php

---
+++ corrige@.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Institut Français | Algérie</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <link rel="stylesheet" href="css/dinot.css">
+        <link rel="stylesheet" href="css/fa/font-awesome.min.css">
+        <link rel="stylesheet" href="css/style.css">
+		<!-- JavaScript Includes -->
+		<script src="js/jquery.js"></script>
+    	<script src="js/bootstrap.min.js"></script>
+        <script src="js/sortable.js"></script>
+    	<script src="js/bootstrap.min.js"></script>
+		<script src="js/transition.js"></script>
+		<script src="js/collapse.js"></script>
+		<script src="js/dropdown.js"></script>
+		<script src="js/scrollspy.js"></script>  
+		<style>
+		body { padding-top: 50px; }
+		</style>
+		 <!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		</head>
+	<body data-spy="scroll" data-target=".navbar" data-offset="50">
+		<header>
+			<?php include("include/navbar-topfix-cor1.php");
+			include("include/header-login.php");
+			?>
+		</header>
+		<hr>
+		<!--ANCRE NAVBAR-TOPFIX--><div id="sujet"></div>
+		<div class="container">
+			<div class="well">
+				<h1>Fiches d'aide à la réalisation de courriels professionnels</h1>
+				 <h2>Corrigé de la fiche N°@</h2></br>
+					<p>Félicitation ! Vous avez terminé votre @ série d'activités.</p>
+					<p>Voici votre score :</p>	
+					<div class="list-group-item">
+		
+		
+					
+						*
+		
+		
+		
+		
+					</div><!--fin list-group-item-->
+			</div><!--fin well principal-->
+		</div><!--fin container principal-->
+		<hr>
+		<footer><!--ANCRE NAVBAR-TOPFIX--><div id="bottom"></div>
+			<?php include("include/footer.php"); ?> 
+		</footer>
+		<!-- JavaScript Includes -->
+		<script type="text/javascript">
+			$(document).ready(function(){
+				var group = $("ol.phrases").sortable({
+					group: 'phrases',
+
+				onDrop: function (item, container, _super) {
+					$('#ordre').val(group.sortable("serialize").get().join())
+					_super(item, container)
+					},
+				serialize: function (parent, children, isContainer) {
+					return isContainer ? children.join() : parent.val()
+					},
+				})
+			})
+		</script>
+	</body>
+</html>
+		  
