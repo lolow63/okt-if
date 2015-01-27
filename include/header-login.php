a2750cfa6452569ca69458d472905a3eefc57768
lolow63 <laurentbrugiere@yahoo.fr>
2015-01-27 13:04:29 +0100

    modif sur les cols des navs

--- include/header-login.php
+++ include/header-login.php
@@ -6,7 +6,7 @@
         </div>
     </div>
     	<div class="container">
-    	    <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 mainnav">
+    	    <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9 mainnav">
 				<nav class="navbar navbar-default" role="navigation">
                 <!-- Brand and toggle get grouped for better mobile display -->
 					<div class="navbar-header">
@@ -60,7 +60,7 @@
 					</div><!-- /.navbar-collapse -->
 				</nav>   
 			</div>
-           <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+           <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
 				<ul class="nav navbar-nav navbar-default navbar-right">
                     <li><a href="#"><i class="fa fa-user"></i> MON PROFIL</a></li>                     
 				</ul>
