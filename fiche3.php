0e6b259f037d60a1f08dedf094293783fefb3258
lolow63 <laurentbrugiere@yahoo.fr>
2015-01-29 16:13:13 +0100

    Intégration fiche3.php //manque activité3//

---
+++ fiche3.php
@@ -0,0 +1,283 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Institut Français | Algérie</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <link rel="stylesheet" href="css/dinot.css">
+        <link rel="stylesheet" href="css/fa/font-awesome.min.css">
+        <link rel="stylesheet" href="css/style.css">
+		<!-- JavaScript Includes -->
+		<script src="js/jquery.js"></script>
+    	<script src="js/bootstrap.min.js"></script>
+        <script src="js/sortable.js"></script>
+    	<script src="js/bootstrap.min.js"></script>
+		<script src="js/transition.js"></script>
+		<script src="js/collapse.js"></script>
+		<script src="js/dropdown.js"></script>
+		<script src="js/scrollspy.js"></script>  
+		<style>
+		body { padding-top: 50px; }
+		</style>
+		 <!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		</head>
+	<body data-spy="scroll" data-target=".navbar" data-offset="50">
+		<header>
+			<?php include("include/navbar-topfix-2.php");
+			include("include/header-login.php");
+			?>
+		</header>
+		<hr>
+		<!--ANCRE NAVBAR-TOPFIX--><div id="sujet"></div>
+		<div class="container">
+			<div class="well">
+				<h2>FICHE N°3</h2>
+				<div class="row">
+					<div class="text-center well encadre col-xs-12 col-sm-offset-4 col-sm-4 col-sm-offset-4">
+						<h3 class="activite">Thème</h3>
+						<h3 class="remarque">@</h3>
+					</div>
+					<div class="text-center well encadre col-xs-12 col-sm-offset-1 col-sm-10 col-sm-offset-1">
+						<ul class="list-unstyled"><h3 class="activite">Objectifs</h3>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+						</ul>
+					</div>
+				</div>
+				
+				<p class="recoi" id="sujet">Mohammed réçoit le message suivant :</p>
+			
+            <!--BOITE MAIL-->
+				<div class="list-group-item">
+					<div style="float:right"> 
+						<a class="btn btn-info" href="#" title="Accès à votre boite de réception">
+						<i class="fa fa-envelope-o fa-lg"></i> Boite de réception</a>
+
+						<a class="btn btn-primary" href="#" title="Afficher les messages non lus">
+						<i class="fa fa-envelope fa-lg"></i> Non lus </a>
+
+						<a href="#" title="Répondre">
+						<i class="fa fa-reply fa-lg"></i></a>
+
+						<a href="#" title="Répondre à tous">
+						<i class="fa fa-reply-all fa-lg"></i></a>
+
+						<a href="#" title="Faire suivre à vos contacts">
+						<i class="fa fa-share fa-lg"></i></a>
+
+						<a href="#" title="Imprimer">
+						<i class="fa fa-print fa-lg"></i></a>
+					</div>
+					<p><strong>Objet:</strong> Re: votre demande de crédit</p>
+					<div class="list-group-item">
+						<p><strong>De :</strong> service.commercial@<wbr>veh.dz
+							<span><strong>À : </strong>
+								<a class="btn btn-primary btn-xs" href="#" title="Répondre">
+								<i class="fa fa-user fa-lg"></i> Moi</a>
+							</span></br>
+							<strong>Envoyé :</strong> mar. 25/03/15 10:21
+						</p>
+					</div>
+				</div>
+				<div class="list-group-item">
+					<p>Monsieur, </br>
+
+					Faisant suite à votre demande d’information sur la possibilité d’envisager un achat 
+					par crédit bancaire du véhicule CLS 2000, nous sommes au regret de vous informer que 
+					notre société ne fait plus de crédit à la consommation.</br>
+					En effet, nous avons arrêté les ventes par crédit depuis 2009.</br>
+					Tout en vous remerciant par avance de votre intérêt, nous restons à votre entière 
+					disposition pour de plus amples informations.</br>
+					Cordialement</br>
+					Service commercial VEH</p>
+				</div>
+				<div class="list-group-item">
+					<a class="btn btn-primary" href="#" title="Répondre">
+					  <i class="fa fa-reply fa-lg"></i> Répondre</a>
+					<a class="btn btn-info" href="#" title="Répondre à tous">
+					  <i class="fa fa-reply-all fa-lg"></i> Répondre à tous</a>
+					<a class="btn btn-info" href="#" title="Transférer">
+					  <i class="fa fa-share fa-lg"></i> Transférer</a>
+					<a class="btn btn-danger" href="#" title="Supprimer">
+					  <i class="fa fa-trash-o fa-lg"></i> Supprimer</a>
+				</div></br>
+				<!--FIN BOITE MAIL -->
+				
+				<p class="recoi" id="sujet">Mohammed RAÏS décide d’écrire à son chef de service :</p>
+				
+				<!--BOITE MAIL-->
+				<div class="list-group-item">
+					<div style="float:right"> 
+						<a class="btn btn-info" href="#" title="Accès à votre boite de réception">
+						<i class="fa fa-envelope-o fa-lg"></i> Boite de réception</a>
+
+						<a class="btn btn-primary" href="#" title="Afficher les messages non lus">
+						<i class="fa fa-envelope fa-lg"></i> Non lus </a>
+
+						<a href="#" title="Répondre">
+						<i class="fa fa-reply fa-lg"></i></a>
+
+						<a href="#" title="Répondre à tous">
+						<i class="fa fa-reply-all fa-lg"></i></a>
+
+						<a href="#" title="Faire suivre à vos contacts">
+						<i class="fa fa-share fa-lg"></i></a>
+
+						<a href="#" title="Imprimer">
+						<i class="fa fa-print fa-lg"></i></a>
+					</div>
+					<p><strong>Objet:</strong> Sollicitation d’un crédit</p>
+					<div class="list-group-item">
+						<p><strong>De :</strong>
+							<span><a class="btn btn-primary btn-xs" href="#" title="Répondre">
+								<i class="fa fa-user fa-lg"></i> Moi</a>
+							</span>
+							<strong>À : </strong>Rachid HAMDI (rach.hamdi@fargo.dz)
+								</br>
+							<strong>Envoyé :</strong> jeu. 30/03/15 10:21
+						</p>
+					</div>
+				</div>
+				<div class="list-group-item">
+					<p>Bonjour Rachid, </br>
+
+					J’ai décidé d’acheter une voiture pour éviter d’être en retard le matin. Mais mes moyens ne me permettent pas cet achat.</br>
+					En effet, nous avons arrêté les ventes par crédit depuis 2009.</br>
+					
+					On m’a dit que notre comité des oeuvres sociales pourrait m’octroyer une aide financière. Je sollicite, donc, cette aide
+					auprès de vous sachant qu’elle me permettra de me faciliter la vie au quotidien.</br>
+					
+					Je vous remercie infiniment et j’attends votre réponse avec impatience.</br>
+					
+					Cordialement</br>
+					
+					Mohammed RAÏS</p>
+				</div>
+				<div class="list-group-item">
+					<a class="btn btn-success" href="#" title="Envoyer">
+					  <i class="fa fa-send fa-lg"></i> Envoyer</a>
+					<a class="btn btn-info" href="#" title="Enregister dans Brouillons">
+					  <i class="fa fa-save fa-lg"></i>  Enregister dans Brouillons</a>
+					<a class="btn btn-danger" href="#" title="Annuler">
+					  <i class="fa fa-trash-o fa-lg"></i> Annuler</a>
+				</div></br>
+				
+				<!--FIN BOITE MAIL -->
+			                    <!-- FORMULAIRE A TRANSMETTRE -->
+				
+				<form method="post" action="corrige3.php">
+		
+				<h3 id="act1" class="activite">Activité 1</h3>
+				<p><i>Mettez les verbes à l'infinitif au passé composé :</i></p>
+				
+				<ol>
+					<li>Mon collègue <input class="form-control inline-form-control" type="text" name="f3a1q1" id="f3a1q1" placeholder="travailler" size="10" maxlength="20"/> jusqu’à 18h.</li>
+					<li>Vous <input class="form-control inline-form-control" type="text" name="f3a1q2" id="f3a1q2" placeholder="écrire" size="10" maxlength="20"/> plusieurs messages à l’agent commercial.</li>
+					<li>Ta lettre <input class="form-control inline-form-control" type="text" name="f3a1q3" id="f3a1q3" placeholder="arriver" size="10" maxlength="20"/> la semaine dernière.</li>
+					<li>Nous <input class="form-control inline-form-control" type="text" name="f3a1q4" id="f3a1q4" placeholder="répondre" size="15" maxlength="20"/> favorablement à leur demande.</li>
+					<li>Après la réunion, ils <input class="form-control inline-form-control" type="text" name="f3a1q5" id="f3a1q5" placeholder="rester" size="10" maxlength="20"/> au bureau.</li>
+					<li>Le produit <input class="form-control inline-form-control" type="text" name="f3a1q6" id="f3a1q6" placeholder="devenir" size="10" maxlength="20"/> très commercialisés dans le marché.</li>
+				</ol>
+				
+				
+				
+				
+				<h3 id="act2" class="activite">Activité 2</h3>
+				<p class="recoi">Pour aller plus loin</p>
+				<p><i>Déplacez les phrases ci-dessous dans les bonnes cases :</i></p>
+				
+				
+				<div class="row">
+					<div class="col-md-6">
+						<ol class="phrases encours">
+						<li>Peux-tu me donner un coup de pouce ?</li>
+						<li>Je te prie de m’aider pour</li>
+						<li>Réponds-moi stp sur ce sujet ...</li>
+						<li>Ils m’ont dit que ...</li>
+						<li>Je t’envoie le dossier en pj</li>
+						<li>Merci infiniment pour ton aide</li> 
+						</ol>
+						<input type="text" id="ordre" hidden="hidden" name="reponse" value="pas de reponse" /> 
+					</div>
+					<div class="col-md-6">
+						<div class="row">
+							<div class="col-md-6">
+							Langage formel
+								<div class="well">
+								Dépot 1
+								</div>
+							</div>
+							<div class="col-md-6">
+							Langage informel
+								<div id="dropper" class="well">
+								Dépot 2
+								</div>
+							</div>							
+						</div>
+					</div>
+				</div>
+				
+				
+				
+				
+				
+				
+				<h3 id="act3" class="activite">Activité 3</h3>
+				<p><i>Améliorez le style de ce message en barrant les expressions incorrectes :</i></p>
+		
+		
+		
+				<button class="btn btn-success btn-md pull-right" type="submit" hidden="hidden">
+				<i class="fa fa-check-circle"> </i> Voir le corrigé</button></br></br>
+								
+				</form><!-- FIN FORMULAIRE -->
+		
+					
+			</div><!--fin well principal-->
+		</div><!--fin container principal-->
+		<hr>
+		<footer>
+			<?php include("include/footer.php"); ?> 
+		</footer>
+		<!-- JavaScript Includes -->
+		<script type="text/javascript">
+			$(document).ready(function(){
+				var group = $("ol.phrases").sortable({
+					group: 'phrases',
+
+				onDrop: function (item, container, _super) {
+					$('#ordre').val(group.sortable("serialize").get().join())
+					_super(item, container)
+					},
+				serialize: function (parent, children, isContainer) {
+					return isContainer ? children.join() : parent.val()
+					},
+				})
+			})
+		</script>
+		<!--Script pour Scrollspy fluide-->
+		<script>
+			$(function () {
+			$('li>a').on('click', function(e) {
+			e.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+			  scrollTop: $(this.hash).offset().top
+				}, 1000, function(){
+				window.location.hash = hash;
+				});
+			});
+		});
+		</Script>
+	</body>
+</html>
+		  
