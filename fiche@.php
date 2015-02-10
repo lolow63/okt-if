b5928305018be8f85f23db26ba4cc81d46e25d91
lolow63 <laurentbrugiere@yahoo.fr>
2015-02-10 12:36:44 +0100

    Recherche de correctifs pour le javascript sur le navbar top fix

--- fiche@.php
+++ fiche@.php
@@ -15,10 +15,7 @@
 		<style>
 		body { padding-top: 50px; }
 		</style>
-		 <!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+
 		</head>
 	<body data-spy="scroll" data-target=".navbar" data-offset="50">
 		<header>
@@ -176,13 +173,18 @@
 			<?php include("include/footer.php"); ?> 
 		</footer>
 			<!-- JavaScript Includes -->
+			
+					 <!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
 		<script src="js/jquery.js"></script>
-    	<script src="js/bootstrap.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
         <script src="js/sortable.js"></script>
 		<script src="js/transition.js"></script>
 		<script src="js/collapse.js"></script>
 		<script src="js/dropdown.js"></script>
-		<script src="js/scrollspy.js"></script> 
+		<script src="js/scrollspy.js"></script>
 		<!-- JavaScript Sortable -->
 		<script type="text/javascript">
 			$(document).ready(function(){
@@ -198,7 +200,7 @@
 					},
 				})
 			})
-		</script>
+		</script type="text/javascript">
 		<!--Script pour Scrollspy fluide-->
 		<script>
 			$(function () {
