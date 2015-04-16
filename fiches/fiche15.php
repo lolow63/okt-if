8c1a01791701ae72c2725ff6ba32a30271505353
lolow63 <laurentbrugiere@yahoo.fr>
2015-04-16 11:33:56 +0200

    Intégration fiche 15 activité 3 en discution

---
+++ fiches/fiche15.php
@@ -0,0 +1,287 @@
+<!doctype html>
+<html>
+    <head>
+		<title>Fiche 15 | Formation de l'Institut Français Algérie</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<![endif]-->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/bootstrap-theme.css">
+        <link rel="stylesheet" href="../css/responsive.css">
+        <link rel="stylesheet" href="../css/dinot.css">
+        <link rel="stylesheet" href="../css/fa/font-awesome.min.css">
+        <link rel="stylesheet" href="../css/style.css">
+		<link rel="shortcut icon" type="image/x-icon" href="http://www.if-algerie.com/favicon.ico">
+		<link rel="apple-touch-icon" href="http://www.if-algerie.com/touch_icon.png">
+		<style> body { padding-top: 50px; } </style>
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+    </head>
+	<body data-spy="scroll" data-target=".navbar" data-offset="50">
+		<header>
+		<!--NAVBAR-TOPFIX-->
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".js-navbar-scrollspy">
+          <span class="sr-only">Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">FICHE 15</a>
+      </div>
+      <div class="collapse navbar-collapse js-navbar-scrollspy">
+        <ul class="nav navbar-nav">
+          <li class=""><a href="#sujet">SUJET</a></li>
+          <li class=""><a href="#act1">ACTIVITÉ 1</a></li>
+		  <li class=""><a href="#act2">ACTIVITÉ 2</a></li>
+          <li class=""><a href="#act3">ACTIVITÉ 3</a></li>
+          
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+<!--FIN BARRE DE NAVIGATION TOP-FIXE-->
+			<?php include("../include/fiches/header-login.php");?>
+		</header>
+		<hr>
+		<!--ANCRE NAVBAR-TOPFIX-->
+		<div class="container">
+			<div class="well">
+				<h2>FICHE N°15</h2>
+				<div class="row">
+					<div class="text-center well encadre col-xs-12 col-sm-offset-4 col-sm-4 col-sm-offset-4">
+						<h3 class="activite">Thème</h3>
+						<h3 class="remarque">@</h3>
+					</div>
+					<div class="text-center well encadre col-xs-12 col-sm-offset-1 col-sm-10 col-sm-offset-1">
+						<h3 class="activite">Objectifs</h3>
+						<ul class="list-unstyled">
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+						</ul>
+					</div>
+				</div>
+				
+                <p id="sujet" class="recoi">Le service commercial informe M.RAÏS</p>
+		
+				<!--BOITE MAIL-->
+ <div class="row">
+			  <div class="col-md-9 col-md-offset-1">
+				<div class="list-group-item">
+					<div style="float:right"> 
+						<a class="btn btn-info" href="#this" title="Accès à votre boite de réception">
+						<i class="fa fa-envelope-o fa-lg"></i> Boite de réception</a>
+
+						<a class="hidden-xs btn btn-primary" href="#this" title="Afficher les messages non lus">
+						<i class="fa fa-envelope fa-lg"></i> Non lus </a>
+
+						<a href="#this" title="Répondre">
+						<i class="hidden-xs fa fa-reply fa-lg"></i></a>
+
+						<a href="#this" title="Répondre à tous">
+						<i class="hidden-xs fa fa-reply-all fa-lg"></i></a>
+
+						<a href="#this" title="Faire suivre à vos contacts">
+						<i class="hidden-xs fa fa-share fa-lg"></i></a>
+
+						<a href="#this" title="Imprimer">
+						<i class="hidden-xs fa fa-print fa-lg"></i></a>
+					</div>
+					<p><strong>Objet:</strong> information</p>
+					<div class="list-group-item">
+						<p><strong>De :</strong> service.commercial@veh.dz
+							<span><strong>À : </strong>
+							<a class="btn btn-primary btn-xs" href="#this" title="Répondre">
+								<i class="fa fa-user fa-lg"></i> Moi</a>
+							</span><br/>
+							<strong>Envoyé :</strong> mar. 20/04/15 11:48
+						</p>
+					</div>
+				</div>
+				<div class="list-group-item">
+					<p>Cher client, <br/>
+					Faisant suite à votre demande de renseignement, nous vous informons que, selon nos conditions de vente et votre contrat d’achat, vous avez un délai de 10 jours pour signer le formulaire de bénéfice du rabais.<br/>
+					Par ailleurs, nous avons le plaisir de vous informer également que votre véhicule est actuellement disponible dans nos stocks. Par conséquent, nous vous prions de vous acquitter du reste de la somme due.<br/>
+					Vous pourriez, ensuite, vous rapprocher de notre agence afin de récupérer votre commande ; un bon de livraison vous sera alors remis avec tous les papiers nécessaires relatifs à votre véhicule.<br/>
+                    Nous vous espérons content de notre produit et vous souhaite une agréable conduite.<br/>
+                    Nous restons à votre disposition pour tout complément d’information.<br/>
+                    Cordialement.<br/>
+                    Service commercial veh motors</p>
+					
+				</div>
+				<div class="list-group-item">
+					<a class="btn btn-primary" href="#this" title="Répondre">
+					  <i class="fa fa-reply fa-lg"></i> Répondre</a>
+					<a class="hidden-xs btn btn-info" href="#this" title="Répondre à tous">
+					  <i class="fa fa-reply-all fa-lg"></i> Répondre à tous</a>
+					<a class="hidden-xs btn btn-info" href="#this" title="Transférer">
+					  <i class="fa fa-share fa-lg"></i> Transférer</a>
+					<a class="hidden-xs btn btn-danger" href="#this" title="Supprimer">
+					  <i class="fa fa-trash-o fa-lg"></i> Supprimer</a>
+				</div><br/>
+</div></div>
+				<!--FIN BOITE MAIL -->
+                <!-- FORMULAIRE A TRANSMETTRE -->
+				 <form method="post" action="../corriges/corrige15.php">
+				
+                <h3 id="act1" class="activite">Activité 1</h3>
+                <div class="well">
+                    <div class="row">
+                        <div class="col-lg-12 col-xs-12">
+                            <p class="label-info label_activite">Qu’est-il demandé à M.RAÏS ?</p>
+                        </div>
+                        <div class="col-sm-4 col-xs-12">
+                            <input type="radio" class="hidden" value="vous n'avez pas répondu" checked="checked" />
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <input type="radio" id="f15a1q1p1" name="f15a1q1" value="De patienter 10 jours pour bénéficier d’un rabais" />
+                                </span>
+                                <label for="f15a1q1p1" class="form-control">De patienter 10 jours pour bénéficier d’un rabais</label>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 col-xs-12">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <input type="radio" id="f15a1q1p2" name="f15a1q1" value="De payer le reste de la somme due avant 10 jours" />
+                                </span>
+                                <label for="f15a1q1p2" class="form-control">De payer le reste de la somme due avant 10 jours</label>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 col-xs-12">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <input type="radio" id="f15a1q1p3" name="f15a1q1" value="De remplir le formulaire du rabais dans un délai de 10 jours" />
+                                </span>
+                                <label for="f15a1q1p3" class="form-control">De remplir le formulaire du rabais dans un délai de 10 jours</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <h3 id="act2" class="activite">Activité 2</h3>
+                <p><i>Indiquez à quelle voie (passive ou active) sont les phrases suivantes :</i></p>
+                
+                <div class="well well-lg">
+					<div class="row">
+						<ol class="vertical col-md-9">
+							<li>1. Mon message est resté sans réponse.</li>
+						</ol>
+						<div class="btn-group col-md-3" data-toggle="buttons">
+							<label class="btn btn-primary">
+							    <input type="radio" hidden="hidden" name="f15a2q1" value="Vous n'avez pas répondu" checked="checked"/>
+								<input type="radio" name="f15a2q1" id="f15a2q1p1" value="Actif"> Actif
+							</label>
+							<label class="btn btn-primary">
+								<input type="radio" name="f15a2q1" id="f15a2q1p2" value="Passif"> Passif
+							</label>
+						</div>
+					</div>
+					<div class="row">
+						<ol class="vertical col-md-9">
+							<li>2. Ces commandes sont destinées à nos clients VIP.</li>
+						</ol>
+						<div class="btn-group col-md-3" data-toggle="buttons">
+							<label class="btn btn-primary">
+							    <input type="radio" hidden="hidden" name="f15a2q2" value="Vous n'avez pas répondu" checked="checked"/>
+								<input type="radio" name="f15a2q1" id="f15a2q2p1" value="Actif"> Actif
+							</label>
+							<label class="btn btn-primary">
+								<input type="radio" name="f15a2q2" id="f15a2q2p2" value="Passif"> Passif
+							</label>
+						</div>
+					</div>	
+					<div class="row">	
+						<ol class="vertical col-md-9">
+							<li>3. Notre marge de bénéfice est montée de 3%.</li>
+						</ol>
+						<div class="btn-group col-md-3" data-toggle="buttons">
+							<label class="btn btn-primary">
+							<input type="radio" hidden="hidden" name="f15a2q3" value="Vous n'avez pas répondu" checked="checked"/>
+								<input type="radio" name="f15a2q3" id="f15a2q3p1" value="Actif"> Actif
+							</label>
+							<label class="btn btn-primary">
+								<input type="radio" name="f15a2q3" id="f15a2q3p2" value="Passif"> Passif
+							</label>
+						</div>
+					</div>
+					<div class="row">	
+						<ol class="vertical col-md-9">
+							<li>4. Nous sommes agacés par l’infiltration des messages indésirables.</li>
+						</ol>
+						<div class="btn-group col-md-3" data-toggle="buttons">
+							<label class="btn btn-primary">
+							<input type="radio" hidden="hidden" name="f15a2q4" value="Vous n'avez pas répondu" checked="checked"/>
+								<input type="radio" name="f15a2q4" id="f15a2q4p1" value="Actif"> Actif
+							</label>
+							<label class="btn btn-primary">
+								<input type="radio" name="f15a2q4" id="f15a2q4p2"value="Passif" > Passif
+							</label>
+						</div>
+					</div>
+				</div>
+                
+				<h3 id="act3" class="activite">Activité 3</h3>
+                <p><i>Transformez à la voix passive les phrases suivantes:<br/>
+                Exemple : </i>Nous vous remettrons un bon de livraison >>> Un bon de livraison vous sera remis.</p>
+                <div class="well">
+                <div class="row">
+                	<ol class="col-md-6 vertical">
+                        <li>1. Les deux entreprises ont signé un contrat</li>
+                    </ol>
+                    <div class="col-md-6">
+                        <input type="text" id="f15a3q1" class="form-control inline-form-control" name="f15a3q1" size="60" maxlength="100" />
+                   	</div>
+                </div>
+                <div class="row">
+                    <ol class="col-md-6 vertical">    
+                        <li>2. Karim remplacera M. RAÏS à la réunion.</li>
+                    </ol>
+                    <div class="col-md-6">    
+                        <input type="text" id="f15a3q2" class="form-control inline-form-control" name="f15a3q2" size="60" maxlength="100" />
+                    </div>
+                </div>
+                <div class="row">
+                    <ol class="col-md-6 vertical">    
+                        <li>3. Nous vous envoyons les détails très bientôt.</li>
+                    </ol>
+                    <div class="col-md-6">    
+                        <input type="text" id="f15a3q3" class="form-control inline-form-control" name="f15a3q3" size="60" maxlength="100" />
+                    </div>
+                </div>
+                <div class="row">
+                    <ol class="col-md-6 vertical">    
+                        <li>4. Le directeur a annulé sa mission au sud.</li>
+                    </ol>
+                    <div class="col-md-6">    
+                        <input type="text" id="f15a3q4" class="form-control inline-form-control" name="f15a3q4" size="60" maxlength="100" />
+                	</div>
+                </div>
+                </div>  
+                
+					<button class="btn btn-success btn-md pull-right" type="submit" hidden="hidden">
+				<i class="fa fa-check-circle"> </i> Voir le corrigé</button><br/><br/>
+								
+				   </form><!-- FIN FORMULAIRE -->
+		
+					
+			</div><!--fin well principal-->
+		</div><!--fin container principal-->
+		<hr>
+		<footer>
+			<?php include("../include/fiches/footer.php"); ?> 
+		</footer>
+			<!-- JavaScript Includes -->
+		<script src="../js/jquery.js"></script>
+    	<script src="../js/bootstrap.min.js"></script> 
+	</body>
+</html>
+		  		  
