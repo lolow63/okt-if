00ce2907921b674a1a0a3ee4e9c7f9a71cf959cf
lolow63 <laurentbrugiere@yahoo.fr>
2015-04-16 21:46:29 +0200

    intégration fiche 16, activité 3 douteuse

---
+++ fiches/fiche16.php
@@ -0,0 +1,318 @@
+<!doctype html>
+<html>
+    <head>
+		<title>Fiche 16 | Formation de l'Institut Français Algérie</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<![endif]-->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/bootstrap-theme.css">
+        <link rel="stylesheet" href="../css/responsive.css">
+        <link rel="stylesheet" href="../css/dinot.css">
+        <link rel="stylesheet" href="../css/fa/font-awesome.min.css">
+        <link rel="stylesheet" href="../css/style.css">
+		<link rel="shortcut icon" type="image/x-icon" href="http://www.if-algerie.com/favicon.ico">
+		<link rel="apple-touch-icon" href="http://www.if-algerie.com/touch_icon.png">
+		<style> body { padding-top: 50px; } </style>
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+    </head>
+	<body data-spy="scroll" data-target=".navbar" data-offset="50">
+		<header>
+		<!--NAVBAR-TOPFIX-->
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".js-navbar-scrollspy">
+          <span class="sr-only">Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">FICHE 16</a>
+      </div>
+      <div class="collapse navbar-collapse js-navbar-scrollspy">
+        <ul class="nav navbar-nav">
+          <li class=""><a href="#sujet">SUJET</a></li>
+          <li class=""><a href="#act1">ACTIVITÉ 1</a></li>
+		  <li class=""><a href="#act2">ACTIVITÉ 2</a></li>
+          <li class=""><a href="#act3">ACTIVITÉ 3</a></li>
+          
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+<!--FIN BARRE DE NAVIGATION TOP-FIXE-->
+			<?php include("../include/fiches/header-login.php");?>
+		</header>
+		<hr>
+		<!--ANCRE NAVBAR-TOPFIX-->
+		<div class="container">
+			<div class="well">
+				<h2>FICHE N°16</h2>
+				<div class="row">
+					<div class="text-center well encadre col-xs-12 col-sm-offset-4 col-sm-4 col-sm-offset-4">
+						<h3 class="activite">Thème</h3>
+						<h3 class="remarque">@</h3>
+					</div>
+					<div class="text-center well encadre col-xs-12 col-sm-offset-1 col-sm-10 col-sm-offset-1">
+						<h3 class="activite">Objectifs</h3>
+						<ul class="list-unstyled">
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+						</ul>
+					</div>
+				</div>
+		
+				<p id="sujet" class="recoi">M.RAÏS conteste une erreur dans sa commande.</p>
+		
+		<!--BOITE MAIL-->
+
+ <div class="row">
+			  <div class="col-md-9 col-md-offset-1">
+				<div class="list-group-item">
+					<div style="float:right"> 
+						<a class="btn btn-info" href="#this" title="Accès à votre boite de réception">
+						<i class="fa fa-envelope-o fa-lg"></i> Boite de réception</a>
+
+						<a class="hidden-xs btn btn-primary" href="#this" title="Afficher les messages non lus">
+						<i class="fa fa-envelope fa-lg"></i> Non lus </a>
+
+						<a href="#this" title="Répondre">
+						<i class="hidden-xs fa fa-reply fa-lg"></i></a>
+
+						<a href="#this" title="Répondre à tous">
+						<i class="hidden-xs fa fa-reply-all fa-lg"></i></a>
+
+						<a href="#this" title="Faire suivre à vos contacts">
+						<i class="hidden-xs fa fa-share fa-lg"></i></a>
+
+						<a href="#this" title="Imprimer">
+						<i class="hidden-xs fa fa-print fa-lg"></i></a>
+					</div>
+					<p><strong>Objet:</strong> Re : information</p>
+					<div class="list-group-item">
+						<p><strong>De :</strong>
+						<a class="btn btn-primary btn-xs" href="#this" title="Répondre">
+								<i class="fa fa-user fa-lg"></i> Moi</a>
+							<strong>À : </strong> service.commercial@veh.dz
+						</p>
+					</div>
+				</div>
+				<div class="list-group-item">
+					<p>Bonjour, <br/>
+
+					Je reviens vers vous pour vous informer qu’il y a une erreur sur ma commande.
+					En effet, je vous ai passé commande du modèle CLS 2000. Or, il se trouve qu’à la livraison, j’ai récupéré un tout autre modèle.<br/>		
+					Je vous remercie par avance pour votre attention.<br/>
+					En conséquence, je vous demande de bien vouloir rectifier cette erreur dans les meilleurs délais.<br/>
+                    Dans le cas contraire, je vous informe que je serais obligé d’annuler ma commande.<br/>
+					Je vous remercie par avance de me faire un retour rapide.<br/>
+                    Cordialement.<br/>
+					
+					Mohammed RAÏS </p>
+				</div>
+				<div class="list-group-item">
+					<a class="btn btn-success" href="#this" title="Envoyer">
+					  <i class="fa fa-send fa-lg"></i> Envoyer</a>
+					<a class="hidden-xs btn btn-info" href="#this" title="Enregister dans Brouillons">
+					  <i class="fa fa-save fa-lg"></i>  Enregister dans Brouillons</a>
+					<a class="hidden-xs btn btn-danger" href="#this" title="Annuler">
+					  <i class="fa fa-trash-o fa-lg"></i> Annuler</a>
+				</div><br/>
+				</div></div>
+				<!--FIN BOITE MAIL -->
+                
+                <!-- FORMULAIRE A TRANSMETTRE -->
+				 <form method="post" action="../corriges/corrige16.php">
+                 
+                 <h3 id="act1" class="activite">Activité 1</h3>
+				<p><i>Lisez le message et cochez la bonne réponse.</i></p>
+                
+                <div class="well">
+                <div class="row">
+                    <div class="col-lg-12 col-xs-12">
+                        <p class="label-info label_activite">Son objet principal est de :</p>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                        <input type="radio" class="hidden" value="vous n'avez pas répondu" checked="checked" />
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <input type="radio" id="f16a1p1" name="f16a1" value="Modifier la commande" />
+                            </span>
+                            <label for="f16a1p1" class="form-control">Modifier la commande</label>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <input type="radio" id="f16a1p2" name="f16a1" value="Annuler la commande" />
+                            </span>
+                            <label for="f16a1p2" class="form-control">Annuler la commande</label>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <input type="radio" id="f16a1p3" name="f16a1" value="Rectifier la commande" />
+                            </span>
+                            <label for="f16a1p3" class="form-control">Rectifier la commande</label>
+                        </div>
+                    </div>
+                </div>
+                </div>
+		
+        
+        		<h3 id="act2" class="activite">Activité 2</h3>
+                <p><i>Mettez le verbe au passé composé:</i></p>
+        			
+                    <div class="well">
+                    	1. A quelle heure 
+                        <input type="text" id="f16a2q1a"  name="f16a2q1a" class="form-control inline-form-control" placeholder="être" size="10" maxlength="20" />
+                        –vous 
+                        <input type="text" id="f16a2q1b"  name="f16a2q1b" class="form-control inline-form-control" placeholder="rentrer" size="10" maxlength="20" />
+                        hier soir ?<br/>
+                        Comme c’était la fin de travail, nous
+                        <input type="text" id="f16a2q2"  name="f16a2q2" class="form-control inline-form-control" placeholder="rentrer" size="10" maxlength="20" />
+                        le mobilier de bureau.<br/>
+                        2. L’adjointe de direction
+                        <input type="text" id="f16a2q3"  name="f16a2q3" class="form-control inline-form-control" placeholder="montrer" size="10" maxlength="20" />
+                        le courrier à 10h.<br/>
+                        Le chef de service
+                        <input type="text" id="f16a2q4"  name="f16a2q4" class="form-control inline-form-control" placeholder="monter" size="10" maxlength="20" />
+                        à pied par ce que l’ascenseur était en panne.<br/>
+                        3. Nous
+                        <input type="text" id="f16a2q5"  name="f16a2q5" class="form-control inline-form-control" placeholder="retourner" size="15" maxlength="20" />
+                        voir les dossiers pour vérifier une anomalie.<br/>
+                        Ils
+                        <input type="text" id="f16a2q6"  name="f16a2q6" class="form-control inline-form-control" placeholder="retourner" size="10" maxlength="20" />
+                        les fiches pour voir l’autre face.<br/>
+                        4. Tous les passagers
+                        <input type="text" id="f16a2q7"  name="f16a2q7" class="form-control inline-form-control" placeholder="descendre" size="10" maxlength="20" />
+                        de l’avion.<br/>
+                        J'
+                        <input type="text" id="f16a2q8"  name="f16a2q8" class="form-control inline-form-control" placeholder="descendre" size="10" maxlength="20" />
+                        l’escalier en courant pour répondre au téléphone.<br/>
+                        5. Est-ce que tu
+                        <input type="text" id="f16a2q9"  name="f16a2q9" class="form-control inline-form-control" placeholder="passer" size="10" maxlength="20" />
+                        un appel au service technique ?<br/>
+               			Il         
+                        <input type="text" id="f16a2q10"  name="f16a2q10" class="form-control inline-form-control" placeholder="passer" size="10" maxlength="20" />
+                   		par la rue Didouche pour éviter l’embouteillage.
+                    </div>    
+        			
+                    <h3 id="act3" class="activite">Activité 3</h3>
+                    <p><i>En fait, Mohammd RAÏS s’est déjà entretenu avec Adel, son ami d’enfance au sujet du crédit pour l’achat du véhicule. Il lui explique dans ce mail pourquoi il veut s’endetter.<br/>
+                     Mettez les verbes de ce message au passé composé.</i></p>
+        				<!--BOITE MAIL-->
+
+ <div class="row">
+			  <div class="col-md-9 col-md-offset-1">
+				<div class="list-group-item">
+					<div style="float:right"> 
+						<a class="btn btn-info" href="#this" title="Accès à votre boite de réception">
+						<i class="fa fa-envelope-o fa-lg"></i> Boite de réception</a>
+
+						<a class="hidden-xs btn btn-primary" href="#this" title="Afficher les messages non lus">
+						<i class="fa fa-envelope fa-lg"></i> Non lus </a>
+
+						<a href="#this" title="Répondre">
+						<i class="hidden-xs fa fa-reply fa-lg"></i></a>
+
+						<a href="#this" title="Répondre à tous">
+						<i class="hidden-xs fa fa-reply-all fa-lg"></i></a>
+
+						<a href="#this" title="Faire suivre à vos contacts">
+						<i class="hidden-xs fa fa-share fa-lg"></i></a>
+
+						<a href="#this" title="Imprimer">
+						<i class="hidden-xs fa fa-print fa-lg"></i></a>
+					</div>
+					<p><strong>Objet:</strong> achat du véhicule</p>
+					<div class="list-group-item">
+						<p><strong>De :</strong>
+						<a class="btn btn-primary btn-xs" href="#this" title="Répondre">
+								<i class="fa fa-user fa-lg"></i> Moi</a>
+							<strong>À : </strong> Adel (adel2000@yahoo.fr)
+						</p>
+					</div>
+				</div>
+				<div class="list-group-item">
+					<p>Salut Adel,<br/>
+
+					J'
+                    <input type="text" id="f16a3q1"  name="f16a3q1" class="form-control inline-form-control" placeholder="avoir" size="10" maxlength="20" />
+                    beaucoup de soucis sans voiture. Par exemple, hier, je n' 
+                    <input type="text" id="f16a3q2"  name="f16a3q2" class="form-control inline-form-control" placeholder="pouvoir" size="10" maxlength="20" />
+                    déposer mes enfants à l’école que vers 09h. J'
+                    <input type="text" id="f16a3q3"  name="f16a3q3" class="form-control inline-form-control" placeholder="devoir" size="10" maxlength="20" />
+                    supplier la directrice pour les laisser entrer. Autre chose, je ne
+                    <input type="text" id="f16a3q4"  name="f16a3q4" class="form-control inline-form-control" placeholder="" size="10" maxlength="20" />
+                    pas
+                    <input type="text" id="f16a3q5"  name="f16a3q5" class="form-control inline-form-control" placeholder="aller" size="10" maxlength="20" />
+                    à la CNAS pour déposer le dossier de maternité de ma femme alors que le dernier délai
+                    <input type="text" id="f16a3q6"  name="f16a3q6" class="form-control inline-form-control" placeholder="fixer" size="10" maxlength="20" />
+                    au 25 du mois courant.<br/>
+                    Voilà quelques raisons qui m'
+                    <input type="text" id="f16a3q7"  name="f16a3q7" class="form-control inline-form-control" placeholder="pousser" size="10" maxlength="20" />
+                    à penser à acheter une voiture.<br/>
+                    Mais toutes les économies que j'
+                    <input type="text" id="f16a3q8"  name="f16a3q8" class="form-control inline-form-control" placeholder="épargner" size="10" maxlength="20" />
+                    n'
+                    <input type="text" id="f16a3q9"  name="f16a3q9" class="form-control inline-form-control" placeholder="" size="10" maxlength="20" />
+                    pas
+                    <input type="text" id="f16a3q10"  name="f16a3q10" class="form-control inline-form-control" placeholder="suffire" size="10" maxlength="20" />
+                    pour verser l’apport initial du véhicule. J' 
+                    <input type="text" id="f16a3q11"  name="f16a3q11" class="form-control inline-form-control" placeholder="" size="10" maxlength="20" />
+                    même
+                    <input type="text" id="f16a3q12"  name="f16a3q12" class="form-control inline-form-control" placeholder="demander" size="10" maxlength="20" />
+                    à ma famille. Tu
+                    <input type="text" id="f16a3q13"  name="f16a3q13" class="form-control inline-form-control" placeholder="voir" size="10" maxlength="20" />
+                    pourquoi je me
+                    <input type="text" id="f16a3q14"  name="f16a3q14" class="form-control inline-form-control" placeholder="endetter" size="10" maxlength="20" />
+                    ?<br/>
+                    J'
+                    <input type="text" id="f16a3q15"  name="f16a3q15" class="form-control inline-form-control" placeholder="penser" size="10" maxlength="20" />
+                    à ma société et je leur
+                    <input type="text" id="f16a3q16"  name="f16a3q16" class="form-control inline-form-control" placeholder="envoyer" size="10" maxlength="20" />
+                    un message pour demander une aide financière.<br/>
+                    J’espère que leur réponse sera favorable.<br/>
+                    Bien à toi.<br/>
+                    Mohammed. </p>
+				</div>
+				<div class="list-group-item">
+					<a class="btn btn-success" href="#this" title="Envoyer">
+					  <i class="fa fa-send fa-lg"></i> Envoyer</a>
+					<a class="hidden-xs btn btn-info" href="#this" title="Enregister dans Brouillons">
+					  <i class="fa fa-save fa-lg"></i>  Enregister dans Brouillons</a>
+					<a class="hidden-xs btn btn-danger" href="#this" title="Annuler">
+					  <i class="fa fa-trash-o fa-lg"></i> Annuler</a>
+				</div><br/>
+				</div></div>
+				<!--FIN BOITE MAIL -->
+                        
+					<button class="btn btn-success btn-md pull-right" type="submit" hidden="hidden">
+				<i class="fa fa-check-circle"> </i> Voir le corrigé</button><br/><br/>
+								
+				   </form><!-- FIN FORMULAIRE -->
+		
+					
+			</div><!--fin well principal-->
+		</div><!--fin container principal-->
+		<hr>
+		<footer>
+			<?php include("../include/fiches/footer.php"); ?> 
+		</footer>
+			<!-- JavaScript Includes -->
+		<script src="../js/jquery.js"></script>
+    	<script src="../js/bootstrap.min.js"></script> 
+	</body>
+</html>
+		  		  
