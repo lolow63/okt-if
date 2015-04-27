b934d31da5676a29ed575519bd1165acfcd44550
lolow63 <laurentbrugiere@yahoo.fr>
2015-04-27 15:04:09 +0200

    Intégration fiche 19

---
+++ fiches/fiche19.php
@@ -0,0 +1,350 @@
+<!doctype html>
+<html>
+    <head>
+		<title>Fiche 19 | Formation de l'Institut Français Algérie</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<![endif]-->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/bootstrap-theme.css">
+        <link rel="stylesheet" href="../css/responsive.css">
+        <link rel="stylesheet" href="../css/dinot.css">
+        <link rel="stylesheet" href="../css/fa/font-awesome.min.css">
+        <link rel="stylesheet" href="../css/style.css">
+		<link rel="shortcut icon" type="image/x-icon" href="http://www.if-algerie.com/favicon.ico">
+		<link rel="apple-touch-icon" href="http://www.if-algerie.com/touch_icon.png">
+		<style> body { padding-top: 50px; } </style>
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+    </head>
+	<body data-spy="scroll" data-target=".navbar" data-offset="50">
+		<header>
+		<!--NAVBAR-TOPFIX-->
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".js-navbar-scrollspy">
+          <span class="sr-only">Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">FICHE 19</a>
+      </div>
+      <div class="collapse navbar-collapse js-navbar-scrollspy">
+        <ul class="nav navbar-nav">
+          <li class=""><a href="#sujet">SUJET</a></li>
+          <li class=""><a href="#act1">ACTIVITÉ 1</a></li>
+		  <li class=""><a href="#act2">ACTIVITÉ 2</a></li>
+          <li class=""><a href="#act3">ACTIVITÉ 3</a></li>
+          <li class=""><a href="#act4">ACTIVITÉ 4</a></li>
+          
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+<!--FIN BARRE DE NAVIGATION TOP-FIXE-->
+			<?php include("../include/fiches/header-login.php");?>
+		</header>
+		<hr>
+		<!--ANCRE NAVBAR-TOPFIX-->
+		<div class="container">
+			<div class="well">
+				<h2>FICHE N°19</h2>
+				<div class="row">
+					<div class="text-center well encadre col-xs-12 col-sm-offset-4 col-sm-4 col-sm-offset-4">
+						<h3 class="activite">Thème</h3>
+						<h3 class="remarque">@</h3>
+					</div>
+					<div class="text-center well encadre col-xs-12 col-sm-offset-1 col-sm-10 col-sm-offset-1">
+						<h3 class="activite">Objectifs</h3>
+						<ul class="list-unstyled">
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+						</ul>
+					</div>
+				</div>
+		
+		<p id="sujet" class="recoi">Message du comité des oeuvres sociales à M.RAIS:</p>
+		
+		<!--BOITE MAIL-->
+ <div class="row">
+			  <div class="col-md-9 col-md-offset-1">
+				<div class="list-group-item">
+					<div style="float:right"> 
+						<a class="btn btn-info" href="#this" title="Accès à votre boite de réception">
+						<i class="fa fa-envelope-o fa-lg"></i> Boite de réception</a>
+
+						<a class="hidden-xs btn btn-primary" href="#this" title="Afficher les messages non lus">
+						<i class="fa fa-envelope fa-lg"></i> Non lus </a>
+
+						<a href="#this" title="Répondre">
+						<i class="hidden-xs fa fa-reply fa-lg"></i></a>
+
+						<a href="#this" title="Répondre à tous">
+						<i class="hidden-xs fa fa-reply-all fa-lg"></i></a>
+
+						<a href="#this" title="Faire suivre à vos contacts">
+						<i class="hidden-xs fa fa-share fa-lg"></i></a>
+
+						<a href="#this" title="Imprimer">
+						<i class="hidden-xs fa fa-print fa-lg"></i></a>
+					</div>
+					<p><strong>Objet:</strong> rappel échéancier de paiement</p>
+					<div class="list-group-item">
+						<p><strong>De :</strong> sal.khadraoui@fargo.dz
+							<span><strong>À : </strong>
+							<a class="btn btn-primary btn-xs" href="#this" title="Répondre">
+								<i class="fa fa-user fa-lg"></i> Moi</a>
+							</span><br/>
+							<strong>Envoyé :</strong> mar. 02/07/15 11:27
+						</p>
+					</div>
+				</div>
+				<div class="list-group-item">
+					<p>Bonjour, <br/>
+					
+                    <p>En date du 10 avril 2015, le comité des oeuvres sociales vous a accordé un microcrédit de 250 000 Da.</p>
+                    <p>Un contrat d’engagement a, par la suite, été signé. Selon les clauses de ce contrat, vous êtes
+                    tenu de vous acquitter mensuellement de la somme de 25 000 Da et cela, comme le prévoit
+                    l’échéancier de remboursement.</p>
+                    <p>A ce jour, votre compte demeure débiteur de la somme de 225 000 Da.<br/>
+                    Par conséquent, nous vous prions de bien vouloir vous rapprocher de notre bureau afin de
+                    régler vos retards de paiement, dans les meilleurs délais.</p>
+					
+					<p>Nous vous remercions par avance.<br/>
+					Salutations distinguées.</p>
+					<p>M. Khadraoui.</p>
+				</div>
+				<div class="list-group-item">
+					<a class="btn btn-primary" href="#this" title="Répondre">
+					  <i class="fa fa-reply fa-lg"></i> Répondre</a>
+					<a class="hidden-xs btn btn-info" href="#this" title="Répondre à tous">
+					  <i class="fa fa-reply-all fa-lg"></i> Répondre à tous</a>
+					<a class="hidden-xs btn btn-info" href="#this" title="Transférer">
+					  <i class="fa fa-share fa-lg"></i> Transférer</a>
+					<a class="hidden-xs btn btn-danger" href="#this" title="Supprimer">
+					  <i class="fa fa-trash-o fa-lg"></i> Supprimer</a>
+				</div><br/>
+</div></div>
+				<!--FIN BOITE MAIL -->
+		
+		<!-- FORMULAIRE A TRANSMETTRE -->
+				 <form method="post" action="../corriges/corrige19.php">
+                
+                <h3 id="act1" class="activite">Activité 1</h3>
+                <p><i>Lisez le message ci-dessus et barrez les informations incorrectes ou non
+mentionnées :</i></p>
+
+<p></p>
+<ul class="well list-unstyled a-rayer">
+	<li class="alinea"><input type="checkbox" id="f19a1qa" name="f19a1qa" /><label for="f19a1qa">a. Le comité des oeuvres sociales doit s’acquitter de la somme de 225 000 Da.</label></li>
+	<li class="alinea"><input type="checkbox" id="f19a1qb" name="f19a1qb" /><label for="f19a1qb">b. M.RAIS peut payer ses dettes quand il veut.</label></li>
+	<li class="alinea"><input type="checkbox" id="f19a1qc" name="f19a1qc" /><label for="f19a1qc">c. M.RAIS a déjà payé une somme du crédit.</label></li>
+	<li class="alinea"><input type="checkbox" id="f19a1qd" name="f19a1qd" /><label for="f19a1qd">d. Le comité des oeuvres sociales invite M.RAIS à régler ses dettes le plus tôt possible.</label></li>
+   	<li class="alinea"><input type="checkbox" id="f19a1qe" name="f19a1qe" /><label for="f19a1qe">e. M.RAIS peut régler par chèque son crédit.</label></li>
+</ul>
+
+
+<h3 id="act2" class="activite">Activité 2</h3>
+                <p><i>Observez l’exemple suivant :</i></p>
+                
+                <p><i>Vous êtes tenu de <strong>vous acquitter</strong> mensuellement de la somme de 225 000 Da.</i></p>
+                <p>Le deuxième <i> vous </i> dans cette phrase fait corps avec le verbe <i> acquitter </i> : on dit qu’il
+                forme avec lui un <strong>verbe pronominal</strong>.</p>
+                
+                <p><i>Conjuguez les verbes pronominaux dans les phrases ci-dessous :</i></p>
+                
+                <div class="well">
+                1. Le chef du comité des oeuvres sociales
+                <input type="text" id="f19a2q1"  name="f19a2q1" class="form-control inline-form-control" placeholder="se rendre" size="15" maxlength="20" />
+                chez son supérieur au sujet de M.RAIS.<br/>
+                2. M.RAIS
+                <input type="text" id="f19a2q2"  name="f19a2q2" class="form-control inline-form-control" placeholder="se rendre compte" size="15" maxlength="20" />
+                qu’il a accumulé des retards de paiement.<br/>
+                3. M.RAIS ne tardera pas à
+                <input type="text" id="f19a2q3"  name="f19a2q3" class="form-control inline-form-control" placeholder="s’engager" size="15" maxlength="20" />
+                pour un contrat d’assurance automobile.<br/>
+                4. Le service commercial Veh Motors
+                <input type="text" id="f19a2q4"  name="f19a2q4" class="form-control inline-form-control" placeholder="se renseigner" size="15" maxlength="20" />
+                si M.RAIS est prêt à
+                <input type="text" id="f19a2q5"  name="f19a2q5" class="form-control inline-form-control" placeholder="se faire" size="15" maxlength="20" />
+                assurer auprès de leur société.<br/>
+                </div>
+                
+                <h3 id="act3" class="activite">Activité 3</h3>
+                <p><i>Trouvez le nom correspondant à chaque définition :</i></p>
+                
+                <div class="well">
+				
+				Personne à qui est due une somme d’argent. <span class="fa fa-arrow-right"></span>
+                <select name="f19a3q1" id="f19a3q1" class="form-control inline-form-control">
+					<option value="Vous n'avez pas répondu" id="f19a3q1p0"></option>
+					<option value="" id="f19a3q1p1">Débiteur</option>
+					<option value="" id="f19a3q1p2">Provision</option>
+					<option value="" id="f19a3q1p3">Créancier</option>
+					<option value="" id="f19a3q1p4">Tireur</option>
+					<option value="" id="f19a3q1p5">Agios</option>
+					<option value="" id="f19a3q1p6">Caution</option>
+				</select><br/>
+				
+				Personne qui doit une somme d’argent. <span class="fa fa-arrow-right"></span>
+				<select name="f19a3q2" id="f19a3q2" class="form-control inline-form-control">
+					<option value="Vous n'avez pas répondu" id="f19a3q2p0"></option>
+					<option value="" id="f19a3q2p1">Débiteur</option>
+					<option value="" id="f19a3q2p2">Provision</option>
+					<option value="" id="f19a3q2p3">Créancier</option>
+					<option value="" id="f19a3q2p4">Tireur</option>
+					<option value="" id="f19a3q2p5">Agios</option>
+					<option value="" id="f19a3q2p6">Caution</option>
+				</select><br/>
+				
+				Personne qui signe le chèque et l’établit au profit d’une autre personne, puis donne l’ordre à sa banque de payer le chèque. <span class="fa fa-arrow-right"></span>
+				<select name="f19a3q3" id="f19a3q3" class="form-control inline-form-control">
+					<option value="Vous n'avez pas répondu" id="f19a3q3p0"></option>
+					<option value="Débiteur" id="f19a3q3p1">Débiteur</option>
+					<option value="Provision" id="f19a3q3p2">Provision</option>
+					<option value="Créancier" id="f19a3q3p3">Créancier</option>
+					<option value="Tireur" id="f19a3q3p4">Tireur</option>
+					<option value="Agios" id="f19a3q3p5">Agios</option>
+					<option value="Caution" id="f19a3q3p6">Caution</option>
+				</select><br/>
+				
+				Somme due à la banque lorsqu’un compte présente un solde négatif pendant un ou plusieurs jours. Le calcul de cette somme s’effectue sur la base des dates de valeur. <span class="fa fa-arrow-right"></span>
+				<select name="f19a3q4" id="f19a3q4" class="form-control inline-form-control">
+					<option value="Vous n'avez pas répondu" id="f19a3q4p0"></option>
+					<option value="Débiteur" id="f19a3q4p1">Débiteur</option>
+					<option value="Provision" id="f19a3q4p2">Provision</option>
+					<option value="Créancier" id="f19a3q4p3">Créancier</option>
+					<option value="Tireur" id="f19a3q4p4">Tireur</option>
+					<option value="Agios" id="f19a3q4p5">Agios</option>
+					<option value="Caution" id="f19a3q4p6">Caution</option>
+				</select><br/>
+				
+				Engagement pris par une personne de se substituer au débiteur, si celui-ci ne paie pas sa dette. <span class="fa fa-arrow-right"></span>
+				<select name="f19a3q5" id="f19a3q5" class="form-control inline-form-control">
+					<option value="Vous n'avez pas répondu" id="f19a3q5p0"></option>
+					<option value="Débiteur" id="f19a3q5p1">Débiteur</option>
+					<option value="Provision" id="f19a3q5p2">Provision</option>
+					<option value="Créancier" id="f19a3q5p3">Créancier</option>
+					<option value="Tireur<" id="f19a3q5p4">Tireur</option>
+					<option value="Agios" id="f19a3q5p5">Agios</option>
+					<option value="Caution" id="f19a3q5p6">Caution</option>
+				</select><br/>
+				
+				Somme disponible et suffisante au crédit du compte et destinée au paiement des opérations réalisées. <span class="fa fa-arrow-right"></span>
+				<select name="f19a3q6" id="f19a3q6" class="form-control inline-form-control">
+					<option value="Vous n'avez pas répondu" id="f19a3q6p0"></option>
+					<option value="Débiteur" id="f19a3q6p1">Débiteur</option>
+					<option value="Provision" id="f19a3q6p2">Provision</option>
+					<option value="Créancier" id="f19a3q6p3">Créancier</option>
+					<option value="Tireur" id="f19a3q6p4">Tireur</option>
+					<option value="Agios" id="f19a3q6p5">Agios</option>
+					<option value="Caution" id="f19a3q6p6">Caution</option>
+				</select><br/>
+				
+				
+                </div>
+                
+                <h3 id="act4" class="activite">Activité 4</h3>
+                <p><i>Remettez dans l'ordre le message d’excuse que doit rédiger Mohammed RAÏS au comité des oeuvres sociales.</i></p>
+				
+				<!--BOITE MAIL-->
+				<div class="row">
+					<div class="col-md-9 col-md-offset-1">
+						<div class="list-group-item">
+							<div style="float:right"> 
+								<a class="btn btn-info" href="#this" title="Accès à votre boite de réception">
+								<i class="fa fa-envelope-o fa-lg"></i> Boite de réception</a>
+
+								<a class="hidden-xs btn btn-primary" href="#this" title="Afficher les messages non lus">
+								<i class="fa fa-envelope fa-lg"></i> Non lus </a>
+
+								<a href="#this" title="Répondre">
+								<i class="hidden-xs fa fa-reply fa-lg"></i></a>
+
+								<a href="#this" title="Répondre à tous">
+								<i class="hidden-xs fa fa-reply-all fa-lg"></i></a>
+
+								<a href="#this" title="Faire suivre à vos contacts">
+								<i class="hidden-xs fa fa-share fa-lg"></i></a>
+
+								<a href="#this" title="Imprimer">
+								<i class="hidden-xs fa fa-print fa-lg"></i></a>
+							</div>
+							<p><strong>Objet:</strong> Re: excuses</p>
+							<div class="list-group-item">
+								<p><strong>De :</strong>
+								<a class="btn btn-primary btn-xs" href="#this" title="Répondre">
+										<i class="fa fa-user fa-lg"></i> Moi</a>
+									<strong>À : </strong> sal.khadraoui@fargo.dz ;
+								</p>
+							</div>
+						</div>
+						<div class="list-group-item">
+							<p>Monsieur,<br/>
+							<ol class="phrases list-unstyled vertical encours">
+								<li value="3"><i class="fa fa-arrows-v"></i> Je vous expliquerai les raisons lors de notre rendez-vous au bureau des oeuvres sociales.</li>
+								<li value="1"><i class="fa fa-arrows-v"></i> Je suis vraiment désolé pour les retards de paiements accusés et je vous remercie pour votre rappel.</li>
+								<li value="4"><i class="fa fa-arrows-v"></i> Par ailleurs, je vous prie de bien vouloir compter sur moi pour m’acquitter des sommes restées impayées jusqu’à présent.</li>
+								<li value="5"><i class="fa fa-arrows-v"></i> Je vous remercie pour votre compréhension.</li>
+								<li value="2"><i class="fa fa-arrows-v"></i> En effet, je n’ai pas alimenté mon compte bancaire depuis trois mois.</li> 
+								<li value="7"><i class="fa fa-arrows-v"></i> M.RAIS.</li>
+								<li value="6"><i class="fa fa-arrows-v"></i> Salutations cordiales.</li>     
+							</ol>
+							<input type="text" id="ordre" hidden="hidden" name="reponse" value="pas de reponse" /> 
+						</div>
+						<div class="list-group-item">
+							<a class="btn btn-success" href="#this" title="Envoyer">
+							  <i class="fa fa-send fa-lg"></i> Envoyer</a>
+							<a class="hidden-xs btn btn-info" href="#this" title="Enregister dans Brouillons">
+							  <i class="fa fa-save fa-lg"></i>  Enregister dans Brouillons</a>
+							<a class="hidden-xs btn btn-danger" href="#this" title="Annuler">
+							  <i class="fa fa-trash-o fa-lg"></i> Annuler</a>
+						</div><br/> 
+					</div>
+				</div>
+				<!--FIN BOITE MAIL -->	
+                
+					<button class="btn btn-success btn-md pull-right" type="submit" hidden="hidden">
+				<i class="fa fa-check-circle"> </i> Voir le corrigé</button><br/><br/>
+								
+				   </form><!-- FIN FORMULAIRE -->
+		
+					
+			</div><!--fin well principal-->
+		</div><!--fin container principal-->
+		<hr>
+		<footer>
+			<?php include("../include/fiches/footer.php"); ?> 
+		</footer>
+			<!-- JavaScript Includes -->
+		<script src="../js/jquery.js"></script>
+    	<script src="../js/bootstrap.min.js"></script>
+		<script src="../js/sortable.js"></script>
+		<!-- JavaScript Sortable -->
+		<script type="text/javascript">
+			$(document).ready(function(){
+				var group = $("ol.phrases").sortable({
+					group: 'phrases',
+
+				onDrop: function (item, container, _super) {
+					$('#ordre').val(group.sortable("serialize").get().join())
+					_super(item, container)
+					},
+				serialize: function (parent, children, isContainer) {
+					return isContainer ? children.join() : parent.val()
+					},
+				})
+			})
+		</script>		
+	</body>
+</html>
+		  		  
