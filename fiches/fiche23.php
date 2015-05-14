dd3283e8fc187d2e2dfea8649fc11ae7bea18a1d
lolow63 <laurentbrugiere@yahoo.fr>
2015-05-14 13:06:03 +0200

    Intégration fiche 23

---
+++ fiches/fiche23.php
@@ -0,0 +1,254 @@
+<!doctype html>
+<html>
+    <head>
+		<title>Fiche 23 | Formation de l'Institut Français Algérie</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<![endif]-->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/bootstrap-theme.css">
+        <link rel="stylesheet" href="../css/responsive.css">
+        <link rel="stylesheet" href="../css/dinot.css">
+        <link rel="stylesheet" href="../css/fa/font-awesome.min.css">
+        <link rel="stylesheet" href="../css/style.css">
+		<link rel="shortcut icon" type="image/x-icon" href="http://www.if-algerie.com/favicon.ico">
+		<link rel="apple-touch-icon" href="http://www.if-algerie.com/touch_icon.png">
+		<style> body { padding-top: 50px; } </style>
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+    </head>
+	<body data-spy="scroll" data-target=".navbar" data-offset="50">
+		<header>
+		<!--NAVBAR-TOPFIX-->
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".js-navbar-scrollspy">
+          <span class="sr-only">Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">FICHE 23</a>
+      </div>
+      <div class="collapse navbar-collapse js-navbar-scrollspy">
+        <ul class="nav navbar-nav">
+          <li class=""><a href="#sujet">SUJET</a></li>
+          <li class=""><a href="#act1">ACTIVITÉ 1</a></li>
+		  <li class=""><a href="#act2">ACTIVITÉ 2</a></li>
+          <li class=""><a href="#act3">ACTIVITÉ 3</a></li>
+          
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+<!--FIN BARRE DE NAVIGATION TOP-FIXE-->
+			<?php include("../include/fiches/header-login.php");?>
+		</header>
+		<hr>
+		<!--ANCRE NAVBAR-TOPFIX-->
+		<div class="container">
+			<div class="well">
+				<h2>FICHE N°23</h2>
+				<div class="row">
+					<div class="text-center well encadre col-xs-12 col-sm-offset-4 col-sm-4 col-sm-offset-4">
+						<h3 class="activite">Thème</h3>
+						<h3 class="remarque">@</h3>
+					</div>
+					<div class="text-center well encadre col-xs-12 col-sm-offset-1 col-sm-10 col-sm-offset-1">
+						<h3 class="activite">Objectifs</h3>
+						<ul class="list-unstyled">
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+						</ul>
+					</div>
+				</div>
+		
+		<!--BOITE MAIL-->
+
+ <div class="row">
+			  <div class="col-md-9 col-md-offset-1">
+				<div class="list-group-item">
+					<div style="float:right"> 
+						<a class="btn btn-info" href="#this" title="Accès à votre boite de réception">
+						<i class="fa fa-envelope-o fa-lg"></i> Boite de réception</a>
+
+						<a class="hidden-xs btn btn-primary" href="#this" title="Afficher les messages non lus">
+						<i class="fa fa-envelope fa-lg"></i> Non lus </a>
+
+						<a href="#this" title="Répondre">
+						<i class="hidden-xs fa fa-reply fa-lg"></i></a>
+
+						<a href="#this" title="Répondre à tous">
+						<i class="hidden-xs fa fa-reply-all fa-lg"></i></a>
+
+						<a href="#this" title="Faire suivre à vos contacts">
+						<i class="hidden-xs fa fa-share fa-lg"></i></a>
+
+						<a href="#this" title="Imprimer">
+						<i class="hidden-xs fa fa-print fa-lg"></i></a>
+					</div>
+					<p><strong>Objet:</strong> Re : Modification de mon contrat d’assurance auto</p>
+					<div class="list-group-item">
+						<p><strong>De :</strong>
+						<a class="btn btn-primary btn-xs" href="#this" title="Répondre">
+								<i class="fa fa-user fa-lg"></i> Moi</a>
+							<strong>À : </strong> assurauto@service.dz
+						</p>
+					</div>
+				</div>
+				<div class="list-group-item">
+					<p>Madame, Monsieur, </p>
+
+										
+					<p>En date du 20/03/2015, j’ai souscrit à un contrat d’assurance auto référencé sous le n°00215014 auprès de votre compagnie.</p>
+                    <p>Or, à la suite d’un accident de la circulation dont j’ai été victime, il m’a semblé nécessaire de revoir certaines clauses de notre contrat.</p>
+                    <p>En effet, notre contrat ne prévoit pas d’assurer tous les risques que peut courir ma nouvelle situation professionnelle. Appelé à me déplacer fréquemment, dans le cadre du travail, vers des zones d’activités industrielles à risques potentiels, je souhaiterais modifier certaines clauses du contrat de façon à pouvoir couvrir cette aggravation du risque.</p>
+                    <p>Je vous serais donc reconnaissant de bien vouloir m’indiquer l’impact de cette modification sur mon contrat.</p>
+                    <p>Je me tiens entièrement à votre disposition pour tout complément d’informations.</p>
+                    <p>Je vous remercie par avance.</p>
+                    <p>Salutations distinguées.<br/>
+					Mohammed RAÏS </p>
+				</div>
+				<div class="list-group-item">
+					<a class="btn btn-success" href="#this" title="Envoyer">
+					  <i class="fa fa-send fa-lg"></i> Envoyer</a>
+					<a class="hidden-xs btn btn-info" href="#this" title="Enregister dans Brouillons">
+					  <i class="fa fa-save fa-lg"></i>  Enregister dans Brouillons</a>
+					<a class="hidden-xs btn btn-danger" href="#this" title="Annuler">
+					  <i class="fa fa-trash-o fa-lg"></i> Annuler</a>
+				</div><br/>
+				</div></div>
+				<!--FIN BOITE MAIL -->
+                <!-- FORMULAIRE A TRANSMETTRE -->
+				 <form method="post" action="../corriges/corrige23.php">
+                 
+                 <h3 id="act1" class="activite">Activité 1</h3>
+					<p><i>Dites si les informations suivantes sont vraies ou fausses :</i></p>
+                    
+                    <div class="well">
+                    
+                    <p>1. M.RAÏS vient de modifier son contrat d’assurance auto.
+                    <span class="inline-form-control btn-group pull-right" data-toggle="buttons">
+                        <input type="radio" class="hidden" id="f23a1q1p0" value="vous n'avez pas répondu" checked="checked" />
+                        <label for="f23a1q1p1" class="btn btn-primary btn-sm"><input type="radio" id="f23a1q1p1" name="f23a1q1p1" value="Vrai"/>Vrai</label>
+                        <label for="f23a1q1p2" class="btn btn-primary btn-sm"><input type="radio" id="f23a1q1p2" name="f23a1q1p2" value="Faux"/>Faux</label>
+                    </span></p><br/>
+                    
+                    <p>2. M.RAÏS veut modifier son contrat parce qu’il a fait un accident.
+                    <span class="inline-form-control btn-group pull-right" data-toggle="buttons">
+                        <input type="radio" class="hidden" id="f23a1q2p0" value="vous n'avez pas répondu" checked="checked" />
+                        <label for="f23a1q2p1" class="btn btn-primary btn-sm"><input type="radio" id="f23a1q2p1" name="f23a1q2p1" value="Vrai"/>Vrai</label>
+                        <label for="f23a1q2p2" class="btn btn-primary btn-sm"><input type="radio" id="f23a1q2p2" name="f23a1q2p2" value="Faux"/>Faux</label>
+                    </span></p><br/>
+                    
+                    <p>3. M.RAÏS est déjà assuré.
+                    <span class="inline-form-control btn-group pull-right" data-toggle="buttons">
+                        <input type="radio" class="hidden" id="f23a1q3p0" value="vous n'avez pas répondu" checked="checked" />
+                        <label for="f23a1q3p1" class="btn btn-primary btn-sm"><input type="radio" id="f23a1q3p1" name="f23a1q3p1" value="Vrai"/>Vrai</label>
+                        <label for="f23a1q3p2" class="btn btn-primary btn-sm"><input type="radio" id="f23a1q3p2" name="f23a1q3p2" value="Faux"/>Faux</label>
+                    </span></p><br/>
+                    
+                    <p>4. Le contrat d’assurance de M.RAÏS ne couvre pas tous les risques.
+                    <span class="inline-form-control btn-group pull-right" data-toggle="buttons">
+                        <input type="radio" class="hidden" id="f23a1q4p0" value="vous n'avez pas répondu" checked="checked" />
+                        <label for="f23a1q4p1" class="btn btn-primary btn-sm"><input type="radio" id="f23a1q4p1" name="f23a1q4p1" value="Vrai"/>Vrai</label>
+                        <label for="f23a1q4p2" class="btn btn-primary btn-sm"><input type="radio" id="f23a1q4p2" name="f23a1q4p2" value="Faux"/>Faux</label>
+                    </span></p><br/>
+                    
+                    </div>
+		
+					<h3 id="act2" class="activite">Activité 2</h3>
+                    <p><i>Voici, dans le désordre, les intentions exprimées dans le courriel. Retrouvez l’ordre des idées.</i></p>
+						<div class="well">
+                        <ol class="phrases list-unstyled vertical encours">
+                        	<li value="2"><i class="fa fa-arrows-v"></i> Exposer le problème.</li>
+                        	<li value="5"><i class="fa fa-arrows-v"></i> Afficher sa disposition.</li>          
+                            <li value="4"><i class="fa fa-arrows-v"></i> Demander des renseignements.</li>
+                            <li value="3"><i class="fa fa-arrows-v"></i> Donner le motif du courriel.</li>
+                            <li value="6"><i class="fa fa-arrows-v"></i> Remercier.</li>
+                            <li value="1"><i class="fa fa-arrows-v"></i> Rappeler le contexte du courriel.</li>
+                        </ol>
+                        <input type="text" id="ordre" hidden="hidden" name="reponse" value="pas de reponse" /> 
+                        </div>	
+                        
+                        
+                        <h3 id="act3" class="activite">Activité 3</h3>
+                        <p><i>Observez cette phrase du courriel :</i></p>
+                        <p><i>Appelé</i> à me déplacer fréquemment, dans le cadre du travail, vers des zones d’activités industrielles à risques potentiels, je souhaiterais modifier certaines clauses du contrat de façon à pouvoir couvrir cette aggravation du risque.</p>
+                        <p>Le mot <i>«appelé»</i> vient du verbe <i>«appeler»</i>. Il est ici employé, en tête de phrase, en tant que participe passé. Il exprime un rapport de cause à effet.</p>
+                        <p><i>En suivant l’exemple, complétez les vides dans les phrases suivantes:</i></p>
+                        
+                        <div class="well">
+                        <p><strong>Exemple:</strong> <i>Puisque</i> je suis appelé à me déplacer fréquemment, dans le cadre du travail, vers des zones d’activités industrielles à risques potentiels, je souhaiterais modifier certaines clauses du contrat de façon à pouvoir couvrir cette aggravation du risque.</p>
+                        <p><i>Appelé</i> à me déplacer fréquemment, dans le cadre du travail, vers des zones d’activités industrielles à risques potentiels, je souhaiterais modifier certaines clauses du contrat de façon à pouvoir couvrir cette aggravation du risque.</p>
+                        
+                        <p><strong>1. </strong><i>Comme</i> le contrat a été modifié par la compagnie d’assurance, il devrait convenir à M.RAÏS.</p>
+                        <p><input type="text" id="f23a3q1" class="form-control inline-form-control" name="f23a3q1" size="10" maxlength="20" />
+                        la compagnie d’assurance,
+                        <input type="text" id="f23a3q2" class="form-control inline-form-control" name="f23a3q2" size="10" maxlength="20" />
+                        devrait convenir à M.RAÏS.</p>
+                        
+                        <p><strong>2. </strong><i>Vu qu’</i> une modification du contrat d’assurance avait été demandée par M.RAÏS, elle lui permettra de couvrir son véhicule contre tous les risques.</p>
+                        <p><input type="text" id="f23a3q3" class="form-control inline-form-control" name="f23a3q3" size="10" maxlength="20" />
+                        M.RAÏS,
+                        <input type="text" id="f23a3q4" class="form-control inline-form-control" name="f23a3q4" size="10" maxlength="20" />
+                        lui permettra de couvrir son véhicule contre tous les risques.</p>
+                        
+                        <p><strong>3. </strong><i>Du moment que</i> la demande de modification a été formulée dans les délais, elle pourrait naturellement être acceptée.</p>
+                        <p><input type="text" id="f23a3q5" class="form-control inline-form-control" name="f23a3q5" size="10" maxlength="20" />
+                        dans les délais,
+                        <input type="text" id="f23a3q6" class="form-control inline-form-control" name="f23a3q6" size="10" maxlength="20" />
+                        pourrait naturellement être acceptée.</p>
+                        
+                        <p><strong>4. </strong><i>Parce que</i> le contrat d’assurance a été modifié, il doit être accompagné de la signature par les deux parties d’un avenant.</p>
+                        <p><input type="text" id="f23a3q7" class="form-control inline-form-control" name="f23a3q7" size="10" maxlength="20" />
+                        ,<input type="text" id="f23a3q8" class="form-control inline-form-control" name="f23a3q8" size="10" maxlength="20" />
+                        doit être accompagné de la signature par les deux parties d’un avenant.</p>
+                        
+                        </div>
+                        
+                        
+                        
+					<button class="btn btn-success btn-md pull-right" type="submit" hidden="hidden">
+				<i class="fa fa-check-circle"> </i> Voir le corrigé</button><br/><br/>
+								
+				   </form><!-- FIN FORMULAIRE -->
+		
+					
+			</div><!--fin well principal-->
+		</div><!--fin container principal-->
+		<hr>
+		<footer>
+			<?php include("../include/fiches/footer.php"); ?> 
+		</footer>
+			<!-- JavaScript Includes -->
+		<script src="../js/jquery.js"></script>
+    	<script src="../js/bootstrap.min.js"></script>
+        <script src="../js/sortable.js"></script> 
+		<!-- JavaScript sortable -->
+		<script type="text/javascript">
+			$(document).ready(function(){
+				var group = $("ol.phrases").sortable({
+					group: 'phrases',
+
+				onDrop: function (item, container, _super) {
+					$('#ordre').val(group.sortable("serialize").get().join())
+					_super(item, container)
+					},
+				serialize: function (parent, children, isContainer) {
+					return isContainer ? children.join() : parent.val()
+					},
+				})
+			})
+		</script>	 
+	</body>
+</html>
+		  		  
---
+++ fiches/fiche24.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+    <head>
+		<title>Fiche 14 | Formation de l'Institut Français Algérie</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<![endif]-->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/bootstrap-theme.css">
+        <link rel="stylesheet" href="../css/responsive.css">
+        <link rel="stylesheet" href="../css/dinot.css">
+        <link rel="stylesheet" href="../css/fa/font-awesome.min.css">
+        <link rel="stylesheet" href="../css/style.css">
+		<link rel="shortcut icon" type="image/x-icon" href="http://www.if-algerie.com/favicon.ico">
+		<link rel="apple-touch-icon" href="http://www.if-algerie.com/touch_icon.png">
+		<style> body { padding-top: 50px; } </style>
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+    </head>
+	<body data-spy="scroll" data-target=".navbar" data-offset="50">
+		<header>
+		<!--NAVBAR-TOPFIX-->
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".js-navbar-scrollspy">
+          <span class="sr-only">Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">FICHE 14</a>
+      </div>
+      <div class="collapse navbar-collapse js-navbar-scrollspy">
+        <ul class="nav navbar-nav">
+          <li class=""><a href="#sujet">SUJET</a></li>
+          <li class=""><a href="#act1">ACTIVITÉ 1</a></li>
+		  <li class=""><a href="#act2">ACTIVITÉ 2</a></li>
+          <li class=""><a href="#act3">ACTIVITÉ 3</a></li>
+          
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+<!--FIN BARRE DE NAVIGATION TOP-FIXE-->
+			<?php include("../include/fiches/header-login.php");?>
+		</header>
+		<hr>
+		<!--ANCRE NAVBAR-TOPFIX-->
+		<div class="container">
+			<div class="well">
+				<h2>FICHE N°14</h2>
+				<div class="row">
+					<div class="text-center well encadre col-xs-12 col-sm-offset-4 col-sm-4 col-sm-offset-4">
+						<h3 class="activite">Thème</h3>
+						<h3 class="remarque">@</h3>
+					</div>
+					<div class="text-center well encadre col-xs-12 col-sm-offset-1 col-sm-10 col-sm-offset-1">
+						<h3 class="activite">Objectifs</h3>
+						<ul class="list-unstyled">
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+							<li class="remarque"><span class="fa fa-angle-double-right"></span>@</li>
+						</ul>
+					</div>
+				</div>
+		
+		
+		
+		
+		
+		
+					<button class="btn btn-success btn-md pull-right" type="submit" hidden="hidden">
+				<i class="fa fa-check-circle"> </i> Voir le corrigé</button><br/><br/>
+								
+				   </form><!-- FIN FORMULAIRE -->
+		
+					
+			</div><!--fin well principal-->
+		</div><!--fin container principal-->
+		<hr>
+		<footer>
+			<?php include("../include/fiches/footer.php"); ?> 
+		</footer>
+			<!-- JavaScript Includes -->
+		<script src="../js/jquery.js"></script>
+    	<script src="../js/bootstrap.min.js"></script> 
+	</body>
+</html>
+		  		  
