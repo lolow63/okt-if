866c55a1f43f21c3ec7b0830a1e664dab08d8936
lolow63 <laurentbrugiere@yahoo.fr>
2015-06-08 16:27:41 +0200

    Intégration espace admin

---
+++ user/espace-admin.php
@@ -0,0 +1,236 @@
+<!doctype html>
+<html>
+    <head>
+		<title>Espace administrateur | Formation de l'Institut Français Algérie</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<![endif]-->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/bootstrap-theme.css">
+        <link rel="stylesheet" href="../css/responsive.css">
+        <link rel="stylesheet" href="../css/dinot.css">
+        <link rel="stylesheet" href="../css/fa/font-awesome.min.css">
+        <link rel="stylesheet" href="../css/style.css">
+		<link rel="shortcut icon" type="image/x-icon" href="http://www.if-algerie.com/favicon.ico">
+		<link rel="apple-touch-icon" href="http://www.if-algerie.com/touch_icon.png">
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+    </head>
+	<body data-spy="scroll" data-target=".navbar" data-offset="50">
+		<header>
+		
+			<?php include("../include/fiches/header-login.php");?>
+		</header>
+		<hr>
+		<!--ANCRE NAVBAR-TOPFIX-->
+		<div class="container">
+			<div class="well">
+				<h2 class="text-center">ESPACE ADMINISTRATEUR</h2>
+				
+					
+                    <h3 class="activite text-center">Effectifs:</h3>
+                    	<div class="row">
+						<ul class="list-group  col-xs-12 col-md-8 col-md-offset-2">
+                            <li class="list-group-item remarque">
+                            <span class="badge">302</span>
+                            Nombres d'inscrits:
+                            </li>
+                            <li class="list-group-item remarque">
+                            <span class="badge">54%</span>
+                            Taux de réussite:
+                            </li>
+                        </ul>
+                        </div>
+                        <h3 class="activite text-center">Répartition géographique:</h3>
+                        	<div class="row">
+                                <ul class="list-group  col-xs-12 col-md-8 col-md-offset-2">
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">42.20%</span>
+                                    Algérie:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">35.92%</span>
+                                    Etats-Unis:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">7.62%</span>
+                                    Non indiqué:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">2.09%</span>
+                                    Chine:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">1.57%</span>
+                                    France:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">1.05%</span>
+                                    Allemagne:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">1.05%</span>
+                                    Royaume-Uni:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">0.97%</span>
+                                    Japon:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">0.97%</span>
+                                    Corée du Sud:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">0.60%</span>
+                                    Canada:
+                                    </li>
+                                </ul>
+                        	</div>
+                        <h3 class="activite text-center">Tranches d'âge:</h3>
+                        <div class="row">
+                                <ul class="list-group  col-xs-12 col-md-8 col-md-offset-2">
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">10%</span>
+                                    Moins de 18 ans:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">20%</span>
+                                    De 18 à 25 ans:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">35%</span>
+                                    De 26 à 35 ans:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">20%</span>
+                                    De 36 à 50 ans:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">15%</span>
+                                    Plus de 50 ans:
+                                    </li>
+                                </ul>
+                            </div>
+                        <h3 class="activite text-center">Catégories socio-professionelles:</h3>
+						<div class="row">
+                                <ul class="list-group  col-xs-12 col-md-8 col-md-offset-2">
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">10%</span>
+                                    Non renseignée:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">3%</span>
+                                    Agriculteur:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">3.5%</span>
+                                    Artisan:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">2%</span>
+                                    Commerçant:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">15%</span>
+                                    Chef d'entreprise:
+                                    </li>
+									<li class="list-group-item remarque">
+                                    <span class="badge">9%</span>
+                                    Profession libérale:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">4%</span>
+                                    Cadre de la fonction publique:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">3.5%</span>
+                                    Cadre d'entreprise:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">8%</span>
+                                    Professionnel de l'enseignement:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">15%</span>
+                                    Professionnel de la santé:
+                                    </li>
+									<li class="list-group-item remarque">
+                                    <span class="badge">7%</span>
+                                    Employé administratifs:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">20%</span>
+                                    Commercial:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">3.5%</span>
+                                    Techniciens:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">2%</span>
+                                    Contremaître, agent de maîtrise:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">1.5%</span>
+                                    Employé de la fonction publique:
+                                    </li>
+									<li class="list-group-item remarque">
+                                    <span class="badge">1%</span>
+                                    Employé de commerce:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">2.5%</span>
+                                    Services aux particuliers:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">3.5%</span>
+                                    Artiste:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">2%</span>
+                                    Ouvriers:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">1.5%</span>
+                                    Retraité:
+                                    </li>
+									<li class="list-group-item remarque">
+                                    <span class="badge">2%</span>
+                                    Étudiant:
+                                    </li>
+                                    <li class="list-group-item remarque">
+                                    <span class="badge">1.5%</span>
+                                    Chômeurs:
+                                    </li>
+                                </ul>
+								
+								<button class="btn btn-lg btn-primary pull-right"><span class="fa fa-refresh"></span> Mettre à jour les statistiques</button>
+                            </div>
+                        
+					
+		
+		
+		
+		
+		
+		
+		
+
+		
+					
+			</div><!--fin well principal-->
+		</div><!--fin container principal-->
+		<hr>
+		<footer>
+			<?php include("../include/fiches/footer.php"); ?> 
+		</footer>
+			<!-- JavaScript Includes -->
+		<script src="../js/jquery.js"></script>
+    	<script src="../js/bootstrap.min.js"></script> 
+	</body>
+</html>
+		  		  
